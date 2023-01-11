c4d73bdb8e9f848a8ea358e9c03453e2f36aa5d2
albuska <109683293+albuska@users.noreply.github.com>
2023-01-11 21:27:29 +0200

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,7 @@
+const inputRef = document.getElementById('name-input');
+
+const titleRef = document.getElementById('name-output');
+
+inputRef.addEventListener('input', (event) => {
+    titleRef.textContent = event.currentTarget.value;
+}); 
