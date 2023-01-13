e9c0d6a15848f57dbeca485b520ccdb8f9410328
albuska <109683293+albuska@users.noreply.github.com>
2023-01-13 20:43:43 +0200

    сorrect task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,10 @@
-const containerFef = document.querySelector('.widget');
-
 const paragraphRef = document.querySelector('.color');
 
 const buttonRef = document.querySelector('.change-color');
 
 buttonRef.addEventListener('click', getRandomHexColor); 
  
-function getRandomHexColor(event) {
+function getRandomHexColor() {
   document.body.style.backgroundColor  = 
   `#${Math.floor(Math.random() * 16777215).toString(16)}`; 
   paragraphRef.textContent = document.body.style.backgroundColor; 
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,8 @@
+const containerRef = document.getElementById('widget');
+console.log("🚀 containerRef", containerRef);
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
