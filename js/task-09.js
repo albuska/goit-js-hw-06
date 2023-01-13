fa1152ecb7cd0fd7c0ec3520177313768859d2fe
albuska <109683293+albuska@users.noreply.github.com>
2023-01-13 21:49:44 +0200

    start task 10

--- js/task-09.js
+++ js/task-09.js
@@ -9,3 +9,19 @@ function getRandomHexColor() {
   `#${Math.floor(Math.random() * 16777215).toString(16)}`; 
   paragraphRef.textContent = document.body.style.backgroundColor; 
 }
+
+// 2 варіант
+// const color = document.querySelector('.color');
+// const btnChangeColor = document.querySelector('.change-color');
+
+// btnChangeColor.addEventListener('click', onChangeColorBtn);
+
+// function onChangeColorBtn() {
+//   document.body.style.backgroundColor = color.textContent = getRandomHexColor();
+// }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
--- js/task-10.js
+++ js/task-10.js
@@ -1,8 +1,33 @@
-const containerRef = document.getElementById('widget');
-console.log("🚀 containerRef", containerRef);
+const inputRef = document.querySelector('#controls > input');
 
+const dataCreateRef = document.querySelector('[data-create]');
 
+const dataDestroyRef = document.querySelector('[data-destroy]');
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const containerBoxesRef = document.getElementById('boxes'); 
+
+inputRef.addEventListener('input', handleCreateNumber);  
+
+function handleCreateNumber(event) { 
+let getInputNumber = Number(event.currentTarget.value);
+getInputNumber = createBoxes();  
 }
+
+dataCreateRef.addEventListener('click', createBoxes); 
+
+function createBoxes(amount) { 
+  console.log(amount);  
+  // const createNewDivs = document.createElement('div'); 
+  
+  // containerBoxesRef.append(); 
+}
+
+dataDestroyRef.addEventListener('click', destroyBoxes); 
+
+function destroyBoxes(event) {
+event.currentTarget.reset(); 
+}
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
