396b79fc75ba6c3ac5f05621072ade6fd0d267cc
albuska <109683293+albuska@users.noreply.github.com>
2023-01-15 14:12:19 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -32,11 +32,16 @@ function onInputBlur(event) {
 //   inputRef.classList.add('invalid'); 
 // }; 
 
+
 // 3 спосіб
-function onInputBlur(event) {
-  if (event.currentTarget.value.length.toString() === input.dataset.length) {
-    inputChangeClass('valid', 'invalid');
-  } else {
-    inputChangeClass('invalid', 'valid');
-  }
-}
+// function onInputBlur(event) {
+//   if (event.currentTarget.value.length.toString() === inputRef.dataset.length) {
+//     inputChangeClass('valid', 'invalid');
+//   } else {
+//     inputChangeClass('invalid', 'valid');
+//   }
+// }
+// function inputChangeClass(classOne, classTwo) {
+//   inputRef.classList.add(classOne);
+//   inputRef.classList.remove(classTwo);
+// }
