d4fb7e6c0c0fa76ce91ee823def15a58e9e71c54
albuska <109683293+albuska@users.noreply.github.com>
2023-01-15 13:59:48 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -6,15 +6,37 @@ inputRef.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
   if (event.currentTarget.value.length === dataRef) {
-    return inputRef.classList.add('valid');
+     inputRef.classList.add('valid');
+     inputRef.classList.remove('invalid');
   } else {
-    return inputRef.classList.add('invalid');
+     inputRef.classList.add('invalid');
   }
 }
 
-// 2 спосіб "Тернарний оператор"
+
+// 2 спосіб
 // function onInputBlur(event) {
-// return  event.currentTarget.value.length === dataRef ?
-//         inputRef.classList.add("valid") :
-//         inputRef.classList.add("invalid");
+//     if (event.currentTarget.value.length === dataRef) {
+//       addClass(); 
+//   } else {
+//       removeClass(); 
+//   }
 // }
+
+// function addClass() {
+//   inputRef.classList.add('valid');
+//   inputRef.classList.remove('invalid');
+// }; 
+
+// function removeClass() {
+//   inputRef.classList.add('invalid'); 
+// }; 
+
+// 3 спосіб
+function onInputBlur(event) {
+  if (event.currentTarget.value.length.toString() === input.dataset.length) {
+    inputChangeClass('valid', 'invalid');
+  } else {
+    inputChangeClass('invalid', 'valid');
+  }
+}
